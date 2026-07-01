e8983d209935355d9c05cfe48701ae9239f836a6
Narayana S <narayana18072005@gmail.com>
2026-07-01 12:28:30 +0530

    Time: 7 ms (44.19%), Space: 9.7 MB (81.98%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.cpp
+++ 0686-repeated-string-match/0686-repeated-string-match.cpp
@@ -1,45 +1,23 @@
 class Solution {
-private:
-    int BASE = 1000000;
 public:
-    int repeatedStringMatch(string A, string B) {
-        if(A == B) return 1;
-        int count = 1;
-        string source = A;
-        while(source.size() < B.size()){
+    int repeatedStringMatch(string a, string b) {
+        int m = a.length(), n = b.length();
+        string repeated = "";
+        int count = 0;
+        while(repeated.size() < n){
+            repeated += a;
             count++;
-            source+=A;
         }
-        if(source == B) return count;
-        if(Rabin_Karp(source,B) != -1) return count;
-        if(Rabin_Karp(source+A,B) != -1) return count+1;
-        return -1;
-    }
-    int Rabin_Karp(string source, string target){
-        if(source == "" or target == "") return -1;
-        int m = target.size();
-        int power = 1;
-        for(int i = 0;i<m;i++){
-            power = (power*31)%BASE;
+
+        if(repeated.find(b) != string::npos){
+            return count;
         }
-        int targetCode = 0;
-        for(int i = 0;i<m;i++){
-            targetCode = (targetCode*31+target[i])%BASE;
-        }
-        int hashCode = 0;
-        for(int i = 0;i<source.size();i++){
-            hashCode = (hashCode*31 + source[i])%BASE;
-            if(i<m-1) continue;
-            if(i>=m){
-                hashCode = (hashCode-source[i-m]*power)%BASE;
-            }
-            if(hashCode<0)
-                hashCode+=BASE;
-            if(hashCode == targetCode){
-                if(source.substr(i-m+1,m) == target)
-                    return i-m+1;
-            }
-        }
+        repeated += a;
+        count++;
+
+        if(repeated.find(b) != string::npos)
+            return count;
+        
         return -1;
     }
 };
