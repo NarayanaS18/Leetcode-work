993d479ac66b299281b6ea35f36005d337365850
Narayana S <narayana18072005@gmail.com>
2026-07-05 17:23:20 +0530

    Time: 19 ms (96.44%), Space: 23.1 MB (32.28%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
 
-    void bfs(int row, int col, vector<vector<int>>& vis, vector<vector<char>>& grid){
+    void bfs(int row, int col, vector<vector<char>>& grid){
         int m = grid.size(), n = grid[0].size();
-        vis[row][col] = 1;
+        grid[row][col] = '0'; //marking as visited in place
         queue<pair<int, int>> q;
         q.push({row, col});
         while(!q.empty()){
             int row = q.front().first;
             int col = q.front().second;
             q.pop();
+
             int drow[] = {-1,0,1,0};
             int dcol[] = {0,1,0,-1};
+
             for(int i=0; i<4; i++){
                 int nrow = row + drow[i];
                 int ncol = col + dcol[i];
+
                 if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n 
-                && grid[nrow][ncol] == '1' && !vis[nrow][ncol]){
-                    vis[nrow][ncol] = '1';
+                && grid[nrow][ncol] == '1'){
+                    grid[nrow][ncol] = '0';// marking as visited before pushing
                     q.push({nrow, ncol});
                 }
             }                    
@@ -26,12 +29,11 @@ public:
 
     int numIslands(vector<vector<char>>& grid) {
         int m = grid.size(), n = grid[0].size();
-        vector<vector<int>> vis(m, vector<int>(n, 0));
         int count = 0;
         for(int row=0; row<m; row++){
             for(int col=0; col<n; col++){
-                if(!vis[row][col] && grid[row][col] == '1'){
-                    bfs(row, col, vis, grid);
+                if(grid[row][col] == '1'){
+                    bfs(row, col, grid);
                     count++;
                 }
             }
