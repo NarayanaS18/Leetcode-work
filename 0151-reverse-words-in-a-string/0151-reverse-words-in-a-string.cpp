0b02385c664efb7917ae3a0ab835b8aabab4ea29
Narayana S <narayana18072005@gmail.com>
2026-07-11 14:18:43 +0530

    Time: 0 ms (100%), Space: 10.2 MB (69.81%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -2,28 +2,24 @@ class Solution {
 public:
     string reverseWords(string s) {
         int n = s.length();
-        int i = n-1;
+        int idx = n-1;
         string ans = "";
-        while(i >= 0){
-            //ignore the spaces at the end
-            while(i >= 0 && s[i] == ' '){
-                i--;
+        while(idx >= 0){
+            while(idx >= 0 && s[idx] == ' '){
+                idx--;
             }
-            //if it reached beginning end the loop
-            if(i < 0) break;
-
-            int right = i; //mark the ending of the word
-
-            //this loop will run till ending to beginning of the word
-            while(i >= 0 && s[i] != ' '){
-                i--;
+            if(idx < 0){
+                break;
             }
-            //if ans is not empty and contains word this will add space between words
+            int right = idx;
+            while(idx >= 0 && s[idx] != ' '){
+                idx--;
+            }
             if(!ans.empty()){
-                ans += ' '; 
-            }
-            //i would be space before word so i+1 and right will be at last letter of word so right-i will give word length
-            ans += s.substr(i+1, right-i);
+                ans += ' ';
+            }  
+            
+            ans += s.substr(idx+1, right-idx);
         }
         return ans;
     }
