15abac6e55c3340ce353c16e721c9f16ad570623
Narayana S <narayana18072005@gmail.com>
2026-07-17 22:12:47 +0530

    Time: 0 ms (100%), Space: 17.2 MB (17.34%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -3,48 +3,48 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         if(grid.empty()) return 0;
         int m = grid.size(), n = grid[0].size();
-        queue<pair<pair<int, int>, int>> q;
+        
+        // vis tracking: 0 = unvisited, 2 = visited/rotten
         vector<vector<int>> vis(m, vector<int>(n, 0));
+        queue<pair<pair<int, int>, int>> q;
         int freshOrange = 0;
-
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
                 if(grid[i][j] == 2){
-                    q.push({{i,j}, 0});
-                    vis[i][j] = 2;
+                    q.push({{i, j}, 0});
+                    vis[i][j] = 2; // Crucial: Mark initial rotten oranges as visited!
                 }
-                else if(grid[i][j] == 1){
+                if(grid[i][j] == 1){
                     freshOrange++;
                 }
             }
         }
-
-        int drow[] = {-1, 0, 1, 0};
-        int dcol[] = {0, 1, 0, -1};
-        int minute = 0, count = 0;
-
+        
+        int time = 0, count = 0;
+        int drow[4] = {-1, 0, 1, 0};
+        int dcol[4] = {0, 1, 0, -1};
+        
         while(!q.empty()){
             int r = q.front().first.first;
             int c = q.front().first.second;
             int t = q.front().second;
             q.pop();
-            minute = max(minute, t);
+            time = max(time, t);
 
-            for(int i=0; i<4; i++){
+            for(int i = 0; i < 4; i++){
                 int nrow = r + drow[i];
                 int ncol = c + dcol[i];
-
-                if(nrow >= 0 && ncol >= 0 && nrow < m && ncol < n &&
-                 grid[nrow][ncol] == 1 && vis[nrow][ncol] == 0){
-                    q.push({{nrow, ncol}, t+1});
-                    vis[nrow][ncol] = 1;
+                
+                if(nrow >= 0 && ncol >= 0 && nrow < m && ncol < n && vis[nrow][ncol] == 0 && grid[nrow][ncol] == 1){
+                    q.push({{nrow, ncol}, t + 1});
+                    vis[nrow][ncol] = 2;
                     count++;
-                 }
-            }            
-
+                }
+            }
         }
-        if(count != freshOrange) return -1;
         
-        return minute;
+        if(count != freshOrange) return -1;
+        return time;
     }
 };
