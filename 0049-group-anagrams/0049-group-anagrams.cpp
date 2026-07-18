389eb91379c593aa1f88553e5daf4e49a2fb8eb8
Narayana S <narayana18072005@gmail.com>
2026-07-18 22:53:48 +0530

    Time: 69 ms (10.1%), Space: 33.3 MB (13.17%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -5,14 +5,22 @@ public:
         unordered_map<string, vector<string>> mp;
 
         for(string& s: strs){
-            string key = s;
-            sort(key.begin(), key.end());
+            vector<int> count(26, 0);
+
+            for(char c : s){
+                count[c -'a']++;
+            }
+
+            string key;
+            for(int num : count){
+                key += to_string(num) + "#";
+            }
             mp[key].push_back(s);
         }
 
         vector<vector<string>> ans;
         for(auto& it : mp){
-            ans.push_back(it.second);
+            ans.push_back(move(it.second));
         }
         return ans;
     }
