a4b3963162c24e557765506ee64af40c0bd92cfc
Narayana S <narayana18072005@gmail.com>
2026-07-21 13:40:12 +0530

    Time: 0 ms (100%), Space: 11.7 MB (89.59%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        int m = ransomNote.size(), n = magazine.size();
-        vector<int> freq(26, 0);
-        for(int i=0; i<n; i++){
-            freq[magazine[i]-'a']++;
+        int freq[26] = {0};
+        for(int c : magazine){
+            freq[c-'a']++;
         }
-
-        for(int j=0; j<m; j++){
-            freq[ransomNote[j]-'a']--;
-            if(freq[ransomNote[j]-'a'] < 0) return false;
+        for(int ch : ransomNote){
+            freq[ch-'a']--;
+            if(freq[ch-'a'] < 0) return false;
         }
         return true;
     }
