59cb8dfa0e4da1596a729316825c6ec518d996f0
Narayana S <narayana18072005@gmail.com>
2026-07-22 21:45:53 +0530

    Time: 11 ms (59.19%), Space: 9.3 MB (78.38%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -3,22 +3,23 @@ public:
     string longestPalindrome(string s) {
         int n = s.length();
         if(s.empty()) return "";
+
         int st = 0, maxlen = 0;
 
         auto expandAroundCorner = [&](int left, int right){
             while(left >= 0 && right < n && s[left] == s[right]){
-                left--; right++;
+                left--;right++;
             }
             int len = right-left-1;
             if(len > maxlen){
                 maxlen = len;
-                st = left + 1;
+                st = left+1;
             }
         };
 
         for(int i=0; i<n; i++){
-            expandAroundCorner(i, i);//odd length palindrome
-            expandAroundCorner(i, i+1);//even length palindrome
+            expandAroundCorner(i, i);//odd center
+            expandAroundCorner(i, i+1);//even center
         }
         return s.substr(st, maxlen);
     }
