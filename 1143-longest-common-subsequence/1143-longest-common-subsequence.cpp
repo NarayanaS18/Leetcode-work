4c63ab1327838fbc3bd4306b1c657e0e38b153aa
Narayana S <narayana18072005@gmail.com>
2026-07-22 21:57:44 +0530

    Time: 24 ms (71.72%), Space: 27.3 MB (78.5%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -3,31 +3,33 @@ public:
 
     int f(int i, int j, string s1, string s2, vector<vector<int>>& dp){
         if(i == 0 || j == 0) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
+        
         if(s1[i-1] == s2[j-1]){
-            return 1 + f(i-1, j-1, s1, s2, dp);
+            return dp[i][j] = 1 + f(i-1, j-1, s1, s2, dp);
         }
+        if(dp[i][j] != -1) return dp[i][j];
         return dp[i][j] = max(f(i-1, j, s1, s2, dp), f(i, j-1, s1, s2, dp));
     }
 
     int longestCommonSubsequence(string s1, string s2) {
         int m = s1.length(), n = s2.length();
-        vector<int> prev(n+1, 0);
-        prev[0] = 0;
-
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+        for(int i=0; i<=m; i++){
+            dp[i][0] = 0;
+        }
+        for(int j=0; j<=n; j++){
+            dp[0][j] = 0;
+        }
         for(int i=1; i<=m; i++){
-            vector<int> cur(n+1, 0);
-            cur[0] = 0;
             for(int j=1; j<=n; j++){
                 if(s1[i-1] == s2[j-1]){
-                    cur[j] = 1 + prev[j-1];
+                    dp[i][j] = 1 + dp[i-1][j-1];
                 }
                 else{
-                    cur[j] = max(prev[j], cur[j-1]);
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
                 }
             }
-            prev = cur;
         }
-        return prev[n];
+        return dp[m][n];
     }
 };
