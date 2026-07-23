2c31e777175bccd28bf6affe3c3610ff089f469a
Narayana S <narayana18072005@gmail.com>
2026-07-23 12:53:56 +0530

    Time: 16 ms (88.6%), Space: 9.2 MB (93.77%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -3,11 +3,10 @@ public:
 
     int f(int i, int j, string s1, string s2, vector<vector<int>>& dp){
         if(i == 0 || j == 0) return 0;
-        
-        if(s1[i-1] == s2[j-1]){
-            return dp[i][j] = 1 + f(i-1, j-1, s1, s2, dp);
-        }
         if(dp[i][j] != -1) return dp[i][j];
+        if(s1[i-1] == s2[j-1]){
+            return 1 + f(i-1, j-1, s1, s2, dp);
+        }
         return dp[i][j] = max(f(i-1, j, s1, s2, dp), f(i, j-1, s1, s2, dp));
     }
 
