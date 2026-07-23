83df2f1a879dd0aaddb67ddabdce6b4fdaafda41
Narayana S <narayana18072005@gmail.com>
2026-07-23 14:09:01 +0530

    Time: 0 ms (100%), Space: 8.9 MB (35.38%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -3,24 +3,18 @@ public:
     int lengthOfLastWord(string s) {
         int n = s.length();
         int i = n-1;
-        string word = "";
-        while(i >= 0){
-            while(i >= 0 && s[i] == ' '){
-                i--;
-            }
-            if(i < 0){
-                break;
-            }
-            int right = i;
-            while(i >= 0 && s[i] != ' '){
-                i--;
-            }
-
-            if(word.empty()){
-                word = s.substr(i+1, right-i);
-            }
+        
+        while(i >= 0 && s[i] == ' '){
+            i--;
+        }
+        if(i < 0){
+            return 0;
+        }
+        int right = i;
+        while(i >= 0 && s[i] != ' '){
+            i--;
         }
 
-        return word.length();
+        return right - i;;
     }
 };
