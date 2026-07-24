919de1d6a327500a8f352ec500b2d62fc14cee24
Narayana S <narayana18072005@gmail.com>
2026-07-24 18:02:28 +0530

    Time: 10 ms (98.03%), Space: 90.4 MB (8.61%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.cpp
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int findMaxLength(vector<int>& nums) {
         int n = nums.size();
-        vector<int> temp(2*n + 1, INT_MIN);
-        int maxlen = 0, sum = 0;
+        vector<int> temp(2*n+1, INT_MIN);
         temp[n] = -1;
+        int maxlen = 0, sum = 0;
         for(int i=0; i<n; i++){
             sum += (nums[i] == 0) ? -1 : 1;
-            if(temp[sum + n] >= -1){
-                maxlen = max(maxlen, i - temp[sum+n]);
+            if(temp[sum+n] >= -1){
+                maxlen = max(maxlen, i-temp[sum+n]);
             }
             else{
                 temp[sum+n] = i;
