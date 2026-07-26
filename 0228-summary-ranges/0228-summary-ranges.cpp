227ead8a58a8c1d72253cafee23cfed41a24cb3f
Narayana S <narayana18072005@gmail.com>
2026-07-26 18:07:11 +0530

    Time: 0 ms (100%), Space: 9.2 MB (74.41%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.cpp
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -2,22 +2,22 @@ class Solution {
 public:
     vector<string> summaryRanges(vector<int>& nums) {
         int n = nums.size();
-        vector<string> ans;
         int i = 0;
+        vector<string> ans;
 
         while(i < n){
-            int st = nums[i];
-            int j = i;
-            while(j + 1 < n && nums[j] + 1 == nums[j+1]){
+            int st = nums[i];//this will store begin of range
+            int j = i;//this will used to iterate
+
+            while(j+1 < n && nums[j+1] == nums[j]+1){
                 j++;
             }
             if(nums[j] == st){
                 ans.push_back(to_string(st));
             }
             else{
-                ans.push_back(to_string(st) + "->" + to_string(nums[j]));
+                ans.push_back(to_string(st)+ "->" + to_string(nums[j]));
             }
-
             i = j+1;
         }
         return ans;
