8f746a9175d342b3d87aafb057be0dff2a00d53e
Narayana S <narayana18072005@gmail.com>
2026-07-26 17:19:45 +0530

    Time: 60 ms (33.52%), Space: 52.5 MB (7.02%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -8,20 +8,21 @@ public:
         for(int i=1; i<n; i++){
             prefixSum[i] = prefixSum[i-1] + nums[i];
         }
-        unordered_map<int, int> mp;
+
+        unordered_map<int, int> m;
         int cnt = 0;
         for(int i=0; i<n; i++){
             if(prefixSum[i] == k){
                 cnt++;
             }
             int val = prefixSum[i] - k;
-            if(mp.find(val) != mp.end()){
-                cnt += mp[val];
+            if(m.find(val) != m.end()){
+                cnt += m[val];
             }
-            else if(mp.find(val) == mp.end()){
-                mp[val] = 0;
+            else if(m.find(val) == m.end()){
+                m[val] = 0;
             }
-            mp[prefixSum[i]]++;
+            m[prefixSum[i]]++;
         }
         return cnt;
     }
