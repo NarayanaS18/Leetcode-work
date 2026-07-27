c29efc5b47e0f1f3b8df0047d2d230c5910ad6c5
Narayana S <narayana18072005@gmail.com>
2026-07-27 12:10:42 +0530

    Time: 0 ms (100%), Space: 9.1 MB (58.15%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -2,24 +2,18 @@ class Solution {
 public:
     string addStrings(string num1, string num2) {
         int m = num1.length(), n = num2.length();
-        int i = m-1, j = n-1, carry = 0;
         string ans = "";
+        int i = m-1, j = n-1, carry = 0;
         while(i >= 0 || j >= 0 || carry != 0){
-            int sum = carry;
-
-            if(i >= 0){
-                sum += num1[i]-'0';
-                i--;
-            }
-            if(j >= 0){
-                sum += num2[j]-'0';
-                j--;
-            }
-            ans += to_string(sum%10);
+            int d1 = (i >= 0) ? num1[i]-'0' : 0;
+            int d2 = (j >= 0) ?  num2[j]-'0' : 0;
+            int sum = d1 + d2 + carry;
             carry = sum/10;
+            ans += (sum%10 + '0');
+            i--;
+            j--;
         }
-
         reverse(ans.begin(), ans.end());
-        return ans;        
+        return ans;
     }
 };
