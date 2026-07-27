71a6a69cacc2731f5d206f4c8008c40804de623d
Narayana S <narayana18072005@gmail.com>
2026-07-27 14:13:40 +0530

    Time: 0 ms (100%), Space: 14.9 MB (98.67%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-
-    int f(int idx, bool holding, vector<int>& prices, vector<vector<int>>& dp){
-        int n = prices.size();
-        if(idx >= n) return 0;
-        int max1 = 0, max2 = 0;
-        if(dp[idx][holding] != -1) return dp[idx][holding];
-        if(holding){
-            int sell = prices[idx] + f(idx+2, false, prices, dp);
-            int hold = f(idx+1, true, prices, dp);
-            max1 = max(sell, hold);
-        }
-        else{
-            int buy = -prices[idx] + f(idx+1, true, prices, dp);
-            int rest = f(idx+1, false, prices, dp);
-            max2 = max(buy, rest);
-        }
-        return dp[idx][holding] = max(max1, max2);
-    }
-
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n, vector<int>(2, -1));
-        return f(0, false, prices, dp);
+        if(n == 0) return 0;
+
+        int held = INT_MIN;
+        int sold = 0;
+        int rest = 0;
+
+        for(int price : prices){
+            int prev_held = held;
+            int prev_sold = sold;
+            int prev_rest = rest;
+
+            held = max(prev_held, prev_rest - price);
+            sold = prev_held + price;
+            rest = max(prev_rest, prev_sold);
+        }
+        return max(sold, rest);
     }
 };
