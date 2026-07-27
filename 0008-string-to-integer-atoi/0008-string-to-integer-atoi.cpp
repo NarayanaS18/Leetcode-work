8e5df7e09ea067d2677099ebd0f5af8a9fdff2ab
Narayana S <narayana18072005@gmail.com>
2026-07-27 22:24:20 +0530

    Time: 0 ms (100%), Space: 9.2 MB (21.34%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,37 +1,35 @@
 class Solution {
 public:
 
-    bool isdigit(char ch){
-        if(ch >= '0' && ch <= '9') return true;
+    bool isdigit(char c){
+        if(c >= '0' && c <= '9') return true;
         return false;
     }
 
     int myAtoi(string s) {
-        int n = s.length();
-        int i = 0, sign = 1;
+        int n = s.length(), i = 0, sign = 1;
         long long ans = 0;
+        
         while(i < n && s[i] == ' '){
             i++;
         }
         if(i == n) return 0;
+
         if(i < n && s[i] == '+') i++;
-        else if(i < n && s[i] == '-'){   
+        else if(i < n && s[i] == '-'){
             sign = -1;
             i++;
         }
-        while(i < n && s[i] == '0'){
-            i++;
-        }
+
         while(i < n && isdigit(s[i])){
-            int d = s[i]-'0';
-            ans = ans*10 + d;
+            int dig = s[i]-'0';
+            ans = ans*10 + dig;
 
             if(sign == -1 && -ans < INT_MIN) return INT_MIN;
             if(sign == 1 && ans > INT_MAX) return INT_MAX;
-
             i++;
         }
-            
-        return ans*sign;
+
+        return sign*ans;
     }
 };
