acf3c0213325b50a986dda116074f9cce4b5893d
Narayana S <narayana18072005@gmail.com>
2026-07-30 15:30:48 +0530

    Time: 18 ms (8.19%), Space: 21.2 MB (7.41%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+
+    bool dfs(int src, int dest, vector<vector<int>>& adjLs, vector<bool> vis){
+        if(src == dest) return true;
+        vis[src] = true;
+
+        for(int it : adjLs[src]){
+            if(!vis[it]){
+                if(dfs(it, dest, adjLs, vis)) return true;
+            }
+        }
+        return false;
+    }
+
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int V = edges.size();
+        vector<vector<int>> adjLs(V+1);
+
+        for(auto& edge : edges){
+            int u = edge[0], v = edge[1];
+
+            if(!adjLs[u].empty() && ! adjLs[v].empty()){
+                vector<bool> vis(V+1, false);
+                if(dfs(u, v, adjLs, vis)){
+                    return edge;
+                }
+            }
+
+            adjLs[u].push_back(v);
+            adjLs[v].push_back(u);
+        }
+        return {};
+    }
+};
