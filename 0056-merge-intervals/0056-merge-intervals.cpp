0a79152a6d9faccd91ad3be8745ff80996e4ead0
Narayana S <narayana18072005@gmail.com>
2026-08-03 14:07:10 +0530

    Time: 2 ms (92.53%), Space: 23.9 MB (59.32%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -6,11 +6,11 @@ public:
         vector<vector<int>> ans;
 
         for(int i=0; i<n; i++){
-            if(ans.empty() || ans[ans.size()-1][1] < intervals[i][0]){
+            if(ans.empty() || ans.back()[1] < intervals[i][0]){
                 ans.push_back(intervals[i]);
             }
             else{
-                ans[ans.size()-1][1] = max(ans[ans.size()-1][1], intervals[i][1]);
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
             }
         }
         return ans;
