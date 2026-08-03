3987ad993b54f3ccfb1ff4b6c17857f5eaf2b82e
Narayana S <narayana18072005@gmail.com>
2026-08-03 15:20:29 +0530

    Time: 0 ms (100%), Space: 13.3 MB (79.85%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -2,36 +2,39 @@ class Solution {
 public:
 
     bool binarySearch(vector<vector<int>>& matrix, int row, int tar){
-        int n = matrix[row].size();
+        int n = matrix[0].size();
         int st = 0, end = n-1;
         while(st <= end){
             int mid = st + (end-st)/2;
+
             if(matrix[row][mid] == tar){
                 return true;
             }
-            if(matrix[row][mid] < tar) st = mid+1;
-            else end = mid-1;
+            else if(matrix[row][mid] < tar){
+                st = mid+1;
+            }
+            else{
+                end = mid-1;
+            }
         }
         return false;
     }
 
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m = matrix.size(), n = matrix[0].size();
-        bool ans = false;
-        int sRow = 0, eRow = m-1;
-        while(sRow <= eRow){
-            int midRow = sRow + (eRow - sRow)/2;
-            if(target >= matrix[midRow][0] && target <= matrix[midRow][n-1]){
-                ans = binarySearch(matrix, midRow, target);
-                break;
+        int sr = 0, er = m-1;
+        while(sr <= er){
+            int mr = sr + (er-sr)/2;
+            if(matrix[mr][0] <= target && matrix[mr][n-1] >= target){
+                return binarySearch(matrix, mr, target);
             }
-            else if(target >= matrix[midRow][n-1]){
-                sRow = midRow+1;
+            else if(matrix[mr][n-1] <= target){
+                sr = mr+1;
             }
             else{
-                eRow = midRow-1;
+                er = mr-1;
             }
         }
-        return ans;
+        return false;
     }
 };
