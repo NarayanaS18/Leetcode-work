8b2fd06a6899bbd2de6b719486f7711db4d800b5
Narayana S <narayana18072005@gmail.com>
2026-08-06 19:54:17 +0530

    Time: 0 ms (100%), Space: 23.7 MB (71.3%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -3,15 +3,34 @@ public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
         vector<int> ans;
-        int mini = (int)n/3 + 1;
-        unordered_map<int, int> mp;
+        int mini = n/3 + 1;
+        int cnt1 = 0, cnt2 = 0;
+        int val1 = 0, val2 = 0;       
         for(int i=0; i<n; i++){
-            mp[nums[i]]++;
-
-            if(mp[nums[i]] == mini) ans.push_back(nums[i]);
-
-            if(ans.size() == 2) break;
+            if(cnt1 == 0 && nums[i] != val2){
+                cnt1 = 1;
+                val1 = nums[i];
+            }
+            else if(cnt2 == 0 && nums[i] != val1){
+                cnt2 = 1;
+                val2 = nums[i];
+            }
+            else if(nums[i] == val1) cnt1++;
+            else if(nums[i] == val2) cnt2++;
+            else{
+                cnt1--;
+                cnt2--;
+            }
         }
+
+        cnt1 = 0, cnt2 = 0;
+        for(int i=0; i<n; i++){
+            if(nums[i] == val1) cnt1++;
+            if(nums[i] == val2) cnt2++;
+        }        
+        if(cnt1 >= mini) ans.push_back(val1);
+        if(cnt2 >= mini && val1 != val2) ans.push_back(val2);
+        
         sort(ans.begin(), ans.end());
 
         return ans;
