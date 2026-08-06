2e23e44c30e4583f3dd9f18d604d1e338e614437
Narayana S <narayana18072005@gmail.com>
2026-08-06 20:06:54 +0530

    Time: 0 ms (100%), Space: 23.8 MB (41.88%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -4,17 +4,33 @@ public:
         int n = nums.size();
         vector<int> ans;
         int mini = n/3 + 1;
-        unordered_map<int, int> m;
+        int cnt1 = 0, cnt2 = 0;
+        int val1 = 0, val2 = 0;
         for(int i=0; i<n; i++){
-            m[nums[i]]++;
-
-            if(m[nums[i]] == mini) ans.push_back(nums[i]);
-
-            if(ans.size() == 2) break;
+            if(cnt1 == 0 && nums[i] != val2){
+                cnt1 = 1;
+                val1 = nums[i];
+            }
+            else if(cnt2 == 0 && nums[i] != val1){
+                cnt2 = 1;
+                val2 = nums[i];
+            }
+            else if(nums[i] == val1) cnt1++;
+            else if(nums[i] == val2) cnt2++;
+            else{
+                cnt1--;
+                cnt2--;
+            }
         }
+
+        cnt1 = 0, cnt2 = 0;
+        for(int i=0; i<n; i++){
+            if(val1 == nums[i]) cnt1++;
+            if(val2 == nums[i]) cnt2++;
+        }
+        if(cnt1 >= mini) ans.push_back(val1);
+        if(cnt2 >= mini && val1 != val2) ans.push_back(val2);
         sort(ans.begin(), ans.end());
         return ans;
-
-
     }
 };
