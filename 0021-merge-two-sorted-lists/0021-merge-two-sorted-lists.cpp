a9f6137fce8f293e0a51b54619327cc3f1f166e4
Narayana S <narayana18072005@gmail.com>
2026-08-24 20:00:33 +0530

    Time: 0 ms (100%), Space: 19.8 MB (8.58%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+
+    ListNode* convertArr2LL(vector<int>& arr){
+        if(arr.empty()) return NULL;
+        ListNode* head = new ListNode(arr[0]);
+        ListNode* mover = head;
+        int n = arr.size();
+        for(int i=1; i<n; i++){
+            ListNode* temp = new ListNode(arr[i]);
+            mover->next = temp;
+            mover = mover->next;
+        }
+        return head;
+    }
+
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        
+        ListNode* temp1 = list1;
+        ListNode* temp2 = list2;
+        vector<int> arr;
+        while(temp1 != NULL){
+            arr.push_back(temp1->val);
+            temp1 = temp1->next;
+        }
+        while(temp2 != NULL){
+            arr.push_back(temp2->val);
+            temp2 = temp2->next;
+        }
+
+        sort(arr.begin(), arr.end());
+        ListNode* newHead = convertArr2LL(arr);
+        return newHead;
+    }
+};
