205c69b2eecf09f9e4a025b9473f23f351a2b0b2
Narayana S <narayana18072005@gmail.com>
2026-09-08 12:29:36 +0530

    Time: 0 ms (100%), Space: 17.5 MB (6.73%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -11,22 +11,7 @@
 class Solution {
 public:
 
-    ListNode* convertArr2LL(vector<int>& arr){
-        int n = arr.size();
-        ListNode* head = new ListNode(arr[0]);
-        ListNode* mover = head;
-
-        for(int i=1; i<n; i++){
-            ListNode* temp = new ListNode(arr[i]);
-            mover->next = temp;
-            mover = mover->next;
-        }
-
-        return head;
-    }
-
     void Reverse(vector<int>& arr, int st, int end){
-        int n = arr.size();
         while(st <= end){
             swap(arr[st], arr[end]);
             st++;
@@ -34,17 +19,31 @@ public:
         }
     }
 
-    ListNode* reverseKGroup(ListNode* head, int k) {
-        if(head == NULL || head->next == NULL) return head;
-        ListNode* temp = head;
+    ListNode* convertArr2LL(vector<int>& arr){
+        if(arr.empty()) return NULL;
+        ListNode* head = new ListNode(arr[0]);
+        ListNode* mover = head;
 
+        for(int i=1; i<arr.size(); i++){
+            ListNode* temp = new ListNode(arr[i]);
+            mover->next = temp;
+            mover = mover->next;
+        }
+        return head;
+    }
+
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if(head == NULL || head->next == NULL){
+            return head;
+        }
+
+        ListNode* temp = head;
         vector<int> arr;
         while(temp){
             arr.push_back(temp->val);
             temp = temp->next;
         }
         int n = arr.size();
-        if(arr.empty()) return NULL;
 
         for(int i=0; i+k<=n; i+=k){
             Reverse(arr, i, i+k-1);
