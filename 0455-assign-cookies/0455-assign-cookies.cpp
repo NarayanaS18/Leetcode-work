5eacf1882778912310f585cfc9f046ff4041506a
Narayana S <narayana18072005@gmail.com>
2026-09-19 19:08:13 +0530

    Time: 16 ms (9.93%), Space: 44.9 MB (11.93%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int n = g.size(), m = s.size();
+        int i = 0, j = 0;
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        while(j < m && i < n){
+            if(g[i] <= s[j]){
+                i = i+1;
+            }
+            j = j + 1;
+        }
+        return i;
+    }
+};
