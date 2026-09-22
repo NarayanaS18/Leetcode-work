cc51dacc14be3f8467a9053c0a5b470643a69458
Narayana S <narayana18072005@gmail.com>
2026-09-22 14:34:35 +0530

    Time: 0 ms (100%), Space: 8.1 MB (74.52%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,23 +1,41 @@
 class Solution {
 public:
-    bool f(string s, int idx, int cnt, vector<vector<int>>& dp){
-        int n = s.length();
-        if(cnt < 0) return false;
-        if(idx == n) return (cnt == 0);
-        if(dp[idx][cnt] != -1) return dp[idx][cnt];
-        if(s[idx] == '('){
-            return dp[idx][cnt] = f(s, idx+1, cnt+1, dp);
-        }
-        if(s[idx] == ')'){
-            return dp[idx][cnt] = f(s, idx+1, cnt-1, dp);
-        }
+    // bool f(string s, int idx, int cnt, vector<vector<int>>& dp){
+    //     int n = s.length();
+    //     if(cnt < 0) return false;
+    //     if(idx == n) return (cnt == 0);
+    //     if(dp[idx][cnt] != -1) return dp[idx][cnt];
+    //     if(s[idx] == '('){
+    //         return dp[idx][cnt] = f(s, idx+1, cnt+1, dp);
+    //     }
+    //     if(s[idx] == ')'){
+    //         return dp[idx][cnt] = f(s, idx+1, cnt-1, dp);
+    //     }
 
-        return dp[idx][cnt] = f(s, idx+1, cnt+1, dp) || f(s, idx+1, cnt-1, dp) || f(s, idx+1, cnt, dp);
-    }
+    //     return dp[idx][cnt] = f(s, idx+1, cnt+1, dp) || f(s, idx+1, cnt-1, dp) || f(s, idx+1, cnt, dp);
+    // }
 
     bool checkValidString(string s) {
         int n = s.length();
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-        return f(s, 0, 0, dp);
+        int mincnt = 0, maxcnt = 0;
+
+        for(int i=0; i<n; i++){
+            if(s[i] == '('){
+                mincnt++;
+                maxcnt++;
+            }
+            else if(s[i] == ')'){
+                mincnt--;
+                maxcnt--;
+            }
+            else{
+                mincnt--;
+                maxcnt++;
+            }
+
+            if(mincnt < 0) mincnt = 0;
+            if(maxcnt < 0) return false;
+        }
+        return mincnt == 0;
     }
 };
