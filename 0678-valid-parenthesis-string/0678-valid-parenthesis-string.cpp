ff0f1fc9d2850d381e8c43de93ec0e73285664a9
Narayana S <narayana18072005@gmail.com>
2026-09-22 14:41:53 +0530

    Time: 0 ms (100%), Space: 8.1 MB (47.59%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,20 +1,5 @@
 class Solution {
 public:
-    // bool f(string s, int idx, int cnt, vector<vector<int>>& dp){
-    //     int n = s.length();
-    //     if(cnt < 0) return false;
-    //     if(idx == n) return (cnt == 0);
-    //     if(dp[idx][cnt] != -1) return dp[idx][cnt];
-    //     if(s[idx] == '('){
-    //         return dp[idx][cnt] = f(s, idx+1, cnt+1, dp);
-    //     }
-    //     if(s[idx] == ')'){
-    //         return dp[idx][cnt] = f(s, idx+1, cnt-1, dp);
-    //     }
-
-    //     return dp[idx][cnt] = f(s, idx+1, cnt+1, dp) || f(s, idx+1, cnt-1, dp) || f(s, idx+1, cnt, dp);
-    // }
-
     bool checkValidString(string s) {
         int n = s.length();
         int mincnt = 0, maxcnt = 0;
@@ -32,10 +17,9 @@ public:
                 mincnt--;
                 maxcnt++;
             }
-
             if(mincnt < 0) mincnt = 0;
             if(maxcnt < 0) return false;
         }
-        return mincnt == 0;
+        return (mincnt == 0);
     }
 };
